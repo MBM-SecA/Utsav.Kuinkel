5671bf3fcb36c08f46f5d69d7df137b7cdc6dd7b
Utsav Kuinkel <utshaav583@gmail.com>
2020-10-11 23:55:08 +0545

    Names Edited

--- EmployeeManagement/Models/Persons.cs
+++ EmployeeManagement/Models/Persons.cs
@@ -12,13 +12,13 @@ public class Person
     public static List<Person> GetPerson()
     {
         Person empl1 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 1 };
-        Person empl2 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 2 };
-        Person empl3 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 3 };
-        Person empl4 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 4 };
-        Person empl5 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 5 };
-        Person empl6 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 6 };
-        Person empl7 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 7 };
-        Person empl8 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 8 };
+        Person empl2 = new Person() { FirstName = "Hari", LastName = "Bahadur", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 2 };
+        Person empl3 = new Person() { FirstName = "Suntali", LastName = "Orangi", Address = "Mulpani", Gender = 'F', Salary = 100.0, Id = 3 };
+        Person empl4 = new Person() { FirstName = "Upasana", LastName = "Kuinkel", Address = "Mulpani", Gender = 'F', Salary = 100.0, Id = 4 };
+        Person empl5 = new Person() { FirstName = "Dilip", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 5 };
+        Person empl6 = new Person() { FirstName = "Dikshya", LastName = "Kuinkel", Address = "Mulpani", Gender = 'F', Salary = 100.0, Id = 6 };
+        Person empl7 = new Person() { FirstName = "Saurish", LastName = "Aryal", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 7 };
+        Person empl8 = new Person() { FirstName = "Nivriti", LastName = "Aryal", Address = "Mulpani", Gender = 'F', Salary = 100.0, Id = 8 };
 
         return new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6, empl7, empl8 };
     }
