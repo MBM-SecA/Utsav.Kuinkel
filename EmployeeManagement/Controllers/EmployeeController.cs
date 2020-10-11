93a1fe43d022f2c836d34b10fa31626b0b9581d2
Utsav Kuinkel <utshaav583@gmail.com>
2020-10-11 11:43:36 +0545

    Added Employees

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+public class EmployeeController : Controller
+{
+    public ActionResult Index()
+    {
+
+        Person empl1 = new Person()
+        {
+            FirstName = "Utsav",
+            LastName = "Kuinkel",
+            Address = "Mulpani",
+            Gender = 'M',
+            Salary = 100.0,
+        };
+
+        Person empl2 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        Person empl3 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        Person empl4 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        Person empl5 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        Person empl6 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        Person empl7 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        Person empl8 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
+        List<Person> employees = new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6, empl7, empl8 };
+        return View(employees);
+    }
+
+
+}
---
+++ EmployeeManagement/Models/Persons.cs
@@ -0,0 +1,9 @@
+public class Person
+{
+    public double Salary { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public char Gender { get; set; }
+
+}
