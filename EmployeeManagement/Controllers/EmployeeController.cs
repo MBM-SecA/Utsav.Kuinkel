b214ce5f4ed831e7a09e7cab702a7733ac71908c
Utsav Kuinkel <utshaav583@gmail.com>
2020-10-11 20:37:33 +0545

    Created a specific page for Employee

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,34 +1,24 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-
-        Person empl1 = new Person()
-        {
-            FirstName = "Utsav",
-            LastName = "Kuinkel",
-            Address = "Mulpani",
-            Gender = 'M',
-            Salary = 100.0,
-        };
-
-        Person empl2 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl3 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl4 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl5 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl6 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl7 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl8 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        List<Person> employees = new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6, empl7, empl8 };
+        List<Person> employees = Person.GetPerson();
         return View(employees);
     }
 
-    public ActionResult Detail(string firstName)
+    public ActionResult Detail(int id)
     {
-        return View();
+        List<Person> employees = Person.GetPerson();
+        var employee = from person in employees where person.Id == id select person;
+        Console.Write(employee.First());
+        var requiredEmployee = employee.ToList();
+        return View(requiredEmployee);
     }
 
 
--- EmployeeManagement/Models/Persons.cs
+++ EmployeeManagement/Models/Persons.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class Person
 {
     public double Salary { get; set; }
@@ -5,5 +7,20 @@ public class Person
     public string LastName { get; set; }
     public string Address { get; set; }
     public char Gender { get; set; }
+    public int Id { get; set; }
+
+    public static List<Person> GetPerson()
+    {
+        Person empl1 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 1 };
+        Person empl2 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 2 };
+        Person empl3 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 3 };
+        Person empl4 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 4 };
+        Person empl5 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 5 };
+        Person empl6 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 6 };
+        Person empl7 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 7 };
+        Person empl8 = new Person() { FirstName = "Utsav", LastName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0, Id = 8 };
+
+        return new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6, empl7, empl8 };
+    }
 
 }
