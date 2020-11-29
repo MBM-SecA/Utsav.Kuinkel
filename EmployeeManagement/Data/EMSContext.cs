48b78541f7e52361442fcc0f13980a1bc05c892e
Utsav Kuinkel <utshaav583@gmail.com>
2020-11-29 12:52:19 +0545

    Made EMSContext class public

--- EmployeeManagement/Data/EMSContext.cs
+++ EmployeeManagement/Data/EMSContext.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
-class EMSContext : DbContext
+public class EMSContext : DbContext
 {
     public EMSContext(DbContextOptions<EMSContext> options) : base(options)
     {
