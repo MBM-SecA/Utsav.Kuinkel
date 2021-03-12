5b6c22c5805986623d2deeebae7a0a77721cfb32
Utsav Kuinkel <utshaav583@gmail.com>
2021-03-11 20:10:39 -0800

    Session and temp data

--- StateMangement/Controllers/StateController.cs
+++ StateMangement/Controllers/StateController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
 public class StateController: Controller
 {
     public ViewResult First()
@@ -10,8 +12,12 @@ public class StateController: Controller
         //CookieOptions
         Response.Cookies.Append("user",name);
 
-        //1.2 Query String (Query Parameters)
-
+        // 1.2 Query String (Query Parameters)
+        // 2. Server Side
+        // 2.1 Session
+        // 
+        HttpContext.Session.SetString("Address","ktm");
+        
 
         return View();
     }
@@ -19,6 +25,10 @@ public class StateController: Controller
     [HttpGet]
     public ViewResult Second(string name, int age)
     {
+        var userAddress = HttpContext.Session.GetString("Address");
+
+        // 2.2 Temp data
+        TempData["Gaidada"]= "Nischal Upreeti";
         return View();
     }
 
@@ -27,4 +37,16 @@ public class StateController: Controller
     {
         return true;
     }
+
+    public ViewResult Third(string name, int age)
+    {
+        // 2.2 Temp data
+        var gName = TempData["Gaidada"];
+        byte value = 67;
+
+        ViewData["Name1"] = "abc from viewdata";
+        ViewBag.Name = "Gaidada";
+        return View(value);
+    }
+
 }
